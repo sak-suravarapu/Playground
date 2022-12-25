966e8e4155d814e9740a08c8e6e8dee2d2f4fa99
Surya Suravarapu <anilsuravarapu@gmail.com>
2022-12-25 18:13:55 -0500

    Second commit

---
+++ learn_numbers.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Addition:", 1+3)
+	fmt.Println("Subtraction:", 1-3)
+	fmt.Println("Multiplication:", 1*3)
+	fmt.Println("Division:", 10/3)
+	fmt.Println("Division:", 10.0/3)
+	fmt.Println("Exponents:", math.Pow(2, 3))
+}
