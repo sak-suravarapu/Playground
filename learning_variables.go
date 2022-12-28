44cfff91b75725677b457f3065c8cddf6a99fcce
Surya Suravarapu <anilsuravarapu@gmail.com>
2022-12-28 16:34:39 -0500

    commit of variables exercise

---
+++ learning_variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myInt int = 16
+	var val, ok = "yes", true
+	var name string
+	yourInt := 16
+	name = "Keith"
+
+	fmt.Println("myInt is:", myInt)
+	fmt.Println("myInt times two:", myInt*2)
+	fmt.Println("val is:", val)
+	fmt.Println("ok is: ", ok)
+	fmt.Println("yourInt is:", yourInt)
+	fmt.Println("name is:", name)
+}
